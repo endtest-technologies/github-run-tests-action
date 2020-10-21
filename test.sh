5df41fc4195e053e479ec2552264ab5a23350466
Liviu Lupei <50408739+liviulupei@users.noreply.github.com>
2020-10-21 18:21:26 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -34,28 +34,33 @@ do
      #detailedlogs=$( echo "$result" | jq '.detailed_logs' )
      #screenshotsandvideo=$( echo "$result" | jq '.screenshots_and_video' )
      starttime=$( echo "$result" | jq '.start_time' )
-     endtime=$( echo "$result" | jq '.end_time' )   
+     endtime=$( echo "$result" | jq '.end_time' ) 
      
-     echo $testsuitename
-     echo $configuration
-     echo $testcases
-     echo $passed
-     echo $failed
-     echo $errors
-     echo $starttime
-     echo $endtime
+     results=https://endtest.io/results?hash="$hash"
      
-
+     echo Test Suite Name: $testsuitename
+     echo Configuration: $configuration
+     echo Test Cases: $testcases
+     echo Passed: $passed
+     echo Failed: $failed
+     echo Errors: $errors
+     echo Start Time: $starttime
+     echo End Time: $endtime
+     echo Hash: $hash
+     echo Results: $results
+     
      echo ::set-output name=test_suite_name::$( echo "$testsuitename" )
-     echo "::set-output name=configuration::$configuration"
-     echo "::set-output name=test_cases::$testcases"
-     echo "::set-output name=passed::$passed"
-     echo "::set-output name=failed::$failed"
-     echo "::set-output name=errors::$errors"
-     echo "::set-output name=start_time::$starttime"
-     echo "::set-output name=end_time::$endtime"
-     #echo "::set-output name=detailed_logs::$detailedlogs"
-     #echo "::set-output name=screenshots_and_video::$screenshotsandvideo"
+     echo ::set-output name=configuration::$( echo "$configuration" )
+     echo ::set-output name=test_cases::$( echo "$testcases" )
+     echo ::set-output name=passed::$( echo "$passed" )
+     echo ::set-output name=failed::$( echo "$failed" )
+     echo ::set-output name=errors::$( echo "$errors" )
+     echo ::set-output name=start_time::$( echo "$starttime" )
+     echo ::set-output name=end_time::$( echo "$endtime" )
+     echo ::set-output name=detailed_logs::$( echo "$detailedlogs" )
+     echo ::set-output name=screenshots_and_video::$( echo "$screenshotsandvideo" )
+     echo ::set-output name=hash::$( echo "$hash" )
+     echo ::set-output name=results::$( echo "$results" )
      exit 0
   fi
 done
