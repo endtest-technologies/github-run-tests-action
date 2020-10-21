66dbbeb8af1fa28fd01348b73d2d97130e00af06
Liviu Lupei <50408739+liviulupei@users.noreply.github.com>
2020-10-21 18:37:09 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -26,15 +26,15 @@ do
     # Don't print anything
   else
      testsuitename=$( echo $result | jq '.test_suite_name' )
-     configuration=$( echo "$result" | jq '.configuration' )
-     testcases=$( echo "$result" | jq '.test_cases' )
-     passed=$( echo "$result" | jq '.passed' )
-     failed=$( echo "$result" | jq '.failed' )
-     errors=$( echo "$result" | jq '.errors' )
-     #detailedlogs=$( echo "$result" | jq '.detailed_logs' )
-     #screenshotsandvideo=$( echo "$result" | jq '.screenshots_and_video' )
-     starttime=$( echo "$result" | jq '.start_time' )
-     endtime=$( echo "$result" | jq '.end_time' ) 
+     configuration=$( echo $result | jq '.configuration' )
+     testcases=$( echo $result | jq '.test_cases' )
+     passed=$( echo $result | jq '.passed' )
+     failed=$( echo $result | jq '.failed' )
+     errors=$( echo $result | jq '.errors' )
+     detailedlogs=$( echo $result | jq '.detailed_logs' )
+     screenshotsandvideo=$( echo $result | jq '.screenshots_and_video' )
+     starttime=$( echo $result | jq '.start_time' )
+     endtime=$( echo $result | jq '.end_time' ) 
      
      results=https://endtest.io/results?hash="$hash"
      
@@ -49,18 +49,18 @@ do
      echo Hash: $hash
      echo Results: $results
      
-     echo ::set-output name=test_suite_name::$( echo "$testsuitename" )
-     echo ::set-output name=configuration::$( echo "$configuration" )
-     echo ::set-output name=test_cases::$( echo "$testcases" )
-     echo ::set-output name=passed::$( echo "$passed" )
-     echo ::set-output name=failed::$( echo "$failed" )
-     echo ::set-output name=errors::$( echo "$errors" )
-     echo ::set-output name=start_time::$( echo "$starttime" )
-     echo ::set-output name=end_time::$( echo "$endtime" )
-     echo ::set-output name=detailed_logs::$( echo "$detailedlogs" )
-     echo ::set-output name=screenshots_and_video::$( echo "$screenshotsandvideo" )
-     echo ::set-output name=hash::$( echo "$hash" )
-     echo ::set-output name=results::$( echo "$results" )
+     echo ::set-output name=test_suite_name::$( echo $testsuitename )
+     echo ::set-output name=configuration::$( echo $configuration )
+     echo ::set-output name=test_cases::$( echo $testcases )
+     echo ::set-output name=passed::$( echo $passed )
+     echo ::set-output name=failed::$( echo $failed )
+     echo ::set-output name=errors::$( echo $errors )
+     echo ::set-output name=start_time::$( echo $starttime )
+     echo ::set-output name=end_time::$( echo $endtime )
+     echo ::set-output name=detailed_logs::$( echo $detailedlogs )
+     echo ::set-output name=screenshots_and_video::$( echo $screenshotsandvideo )
+     echo ::set-output name=hash::$( echo $hash )
+     echo ::set-output name=results::$( echo $results )
      exit 0
   fi
 done
